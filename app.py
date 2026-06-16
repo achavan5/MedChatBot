7ad1c99393d04378bbe238ed8ba8c560418a7517
achavan5 <avchavan@yahoo.com>
2026-06-16 14:42:46 -0400

    Deploy again

--- app.py
+++ app.py
@@ -61,5 +61,6 @@ def chat():
     print("Response:", response["answer"])
     return str({response["answer"]})
 
+#Fix bind python app to 0.0.0.0 and port 8080 for AWS deployment
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
